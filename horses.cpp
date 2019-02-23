6377cba52fbd4cbb7fc1c7724918dbf9c07666c3
TANISHQ CHAMOLA <36632821+tanishqchamola@users.noreply.github.com>
2019-02-23 22:32:10 +0530

    Add files via upload

---
+++ horses.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+/*
+int main()
+{
+    vector<unsigned int>result;
+    unsigned short int t;
+    cin>>t;
+    
+    for (unsigned short int i=0; i<t; i++)
+    {
+        unsigned short int n=0;
+        unsigned int min=4294967295;
+        cin>>n;
+        unsigned int arr[n];
+        
+        for (unsigned short int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+
+        for (unsigned short int i=0; i<n-1; i++)
+        {
+            for (unsigned short int j=i+1; j<n; j++)
+            {
+                unsigned int diff=0;
+                if (arr[i]>arr[j])
+                {
+                    diff = arr[i]-arr[j];
+                }
+                else
+                {
+                    diff = arr[j]-arr[i];
+                }
+                if (diff<min)
+                {
+                    min = diff;
+                }
+            }
+        }
+        result.push_back(min);
+        
+    }
+    
+    for (unsigned short int i=0; i<t; i++)
+    {
+       cout<<result[i]<<"\n";
+    }
+    
+    return 0;
+}
+*/
---
+++ wcsa.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+/*
+int main()
+{
+    vector<unsigned short int>result;
+    unsigned short int t;
+    cin>>t;
+    
+    for (unsigned short int i=0; i<t; i++)
+    {
+        unsigned short int n,count=0;
+        cin>>n;
+        unsigned short int arr[n];
+        
+        for (unsigned int i=1; i<=n; i++)
+        {
+            cin>>arr[i];
+        }
+        
+        for (unsigned int i=1; i<=n; i++)
+        {
+            if (arr[arr[i]]==i)
+            {
+                count++;
+            }
+        }
+        
+        result.push_back(n-count);
+        
+    }
+    
+    for (unsigned int i=0; i<t; i++)
+    {
+        cout<<result[i]<<"\n";
+    }
+    
+    return 0;
+}
+*/
