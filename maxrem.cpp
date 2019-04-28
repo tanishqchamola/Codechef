27d86191024f879f5acafefc6202252de619cd37
TANISHQ CHAMOLA <36632821+tanishqchamola@users.noreply.github.com>
2019-04-28 22:50:03 +0530

    Add files via upload

---
+++ maxrem.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+int main()
+{
+    unsigned int n;
+    cin>>n;
+    unsigned int arr[n];
+    
+    for (int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    
+    int b=arr[n-2],a=arr[n-1];
+    int j = 2;
+    while(arr[n-j] == arr[n-1])
+    {
+        b = arr[n-(j+1)];
+        j++;
+        if(j>=n)
+        {
+            b = arr[n-2];
+            break;
+        }
+    }
+
+    cout<< b % a;
+    return 0;
+}
+*/
