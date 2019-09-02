f707a807da96fac44c2e92da4d4c5c208b89e8b6
TANISHQ CHAMOLA <36632821+tanishqchamola@users.noreply.github.com>
2019-09-02 20:26:18 +0530

    Add files via upload

---
+++ cequal.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+/*
+int main()
+{
+    unsigned int t;
+    cin>>t;
+    bool result[t];
+    
+    for (int i=0; i<t; i++)
+    {
+        unsigned int n;
+        cin>>n;
+        int arr[n];
+        bool flag = true;
+        
+        for (int j=0; j<n; j++)
+        {
+            int k,num;
+            k=j;
+            cin>>num;
+            arr[j]=num;
+            if(flag==false)
+            {
+                continue;
+            }
+            while(k--)
+            {
+                if (num==arr[k])
+                {
+                    flag = false;
+                }
+            }
+        }
+        result[i]=flag;
+    }
+    for (int i=0; i<t; i++) {
+        if (result[i]==0)
+        {
+            cout<<"Yes\n";
+        }
+        else
+        {
+            cout<<"No\n";
+        }
+    }
+    
+}
+*/
---
+++ stgame.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+int main()
+{
+    unsigned int t;
+    cin>>t;
+    int arr[t];
+    
+    for(int j=0;j<t;j++)
+    {
+        unsigned int n;
+        cin>>n;
+        long int a[n],d[n];
+        
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            cin>>d[i];
+        }
+        sort(a,a+n);
+        sort(d,d+n);
+        
+        long int sum1 = a[n-1]+a[n-2];
+        long int sum2 = d[n-1]+d[n-2];
+        
+        if(sum1>sum2)
+        {
+            arr[j]=1;
+        }
+        else{
+            arr[j]=0;;
+        }
+    }
+    
+    for(int j=0;j<t;j++)
+    {
+        if(arr[j]==1)
+        {
+            cout<<"AAYUSHI\n";
+        }
+        else{
+            cout<<"DAKSH\n";
+        }
+    }
+    return 0;
+}
+*/
